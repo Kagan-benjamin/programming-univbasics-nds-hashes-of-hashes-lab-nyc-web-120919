bbc560a1faf2dc3f24e6f0d409b34e2f8a7d3a1d
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-11-22 19:17:05 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -59,15 +59,4 @@ def naming_system
     
   }
   hash_of_hashes 
-  # Remember:
-  #  Kingdom
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
-  # So, if we have the "Kingdom" node we should be able to "tunnel" into the
-  # HoH all the way to Species!
-
 end
